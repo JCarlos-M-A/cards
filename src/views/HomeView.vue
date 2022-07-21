cf84daf6b895fdf7a062a04c32ea8e3116839950
Carlosjl77 <carlos.alvarez@conveyors-immsa.com>
2022-07-21 09:42:55 -0500

    comite

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import TheWelcome from "@/components/TheWelcome.vue";
+</script>
+
+<template>
+  <main>
+    <h1 class="textito">hola</h1>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+.textito {
+  color: var(--planta);
+  background-color: var(--fondo);
+}
+</style>
